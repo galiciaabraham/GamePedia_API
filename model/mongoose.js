36bd2d7fa9dc51da35628c7830cf5149f5b2f08b
adcostanzi <andres.costanzi@outlook.com>
2024-05-24 18:57:34 -0500

    review get by game id working

--- model/mongoose.js
+++ model/mongoose.js
@@ -1,52 +1,53 @@
-const mongoose =require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const models = {}
+const models = {};
 
 mongoose.connect(process.env.CONNECTION_URI);
 
 const gameSchema = new mongoose.Schema({
-    Name: String,
-    Release: String,
-    Director: String,
-    Composer: String,
-    Series: String,
-    Developer: String,
-    Genre: String
-})
+  Name: String,
+  Release: String,
+  Director: String,
+  Composer: String,
+  Series: String,
+  Developer: String,
+  Genre: String,
+});
 
 const developerSchema = new mongoose.Schema({
-    Name: String,
-    Founded: String,
-    Headquarters: String,
-    President: String,
-    Website:String
-})
+  Name: String,
+  Founded: String,
+  Headquarters: String,
+  President: String,
+  Website: String,
+});
 
 const publisherSchema = new mongoose.Schema({
-    Name: String,
-    Founded: String,
-    Headquarters: String,
-    President: String,
-    Website:String
-})
+  Name: String,
+  Founded: String,
+  Headquarters: String,
+  President: String,
+  Website: String,
+});
 
 const reviewSchema = new mongoose.Schema({
-    UserId: Number,
-    Title: String,
-    Content: String,
-    Rating: Number,
-    Date: String,
-    Verified: String
-})
+  UserId: Number,
+  Title: String,
+  Content: String,
+  Rating: Number,
+  Date: String,
+  Verified: String,
+  GameId: mongoose.Types.ObjectId,
+});
 
-models.reviewModel = mongoose.model('reviews', reviewSchema);
+models.reviewModel = mongoose.model("reviews", reviewSchema);
 
-models.publisherModel = mongoose.model('publishers', publisherSchema);
+models.publisherModel = mongoose.model("publishers", publisherSchema);
 
-models.developerModel = mongoose.model('developers', developerSchema);
- 
-models.gameModel = mongoose.model('games', gameSchema);
+models.developerModel = mongoose.model("developers", developerSchema);
+
+models.gameModel = mongoose.model("games", gameSchema);
 
 module.exports = models;
