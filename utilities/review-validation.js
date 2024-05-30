8b94db4ae7d8ebc08ac69aa00a93a65edf82f8e2
jeffersonabreu <sil21018@byui.edu>
2024-05-30 19:16:02 -0300

    erroe handling and review validation

---
+++ utilities/review-validation.js
@@ -0,0 +1,41 @@
+const { body, param, validationResult } = require('express-validator');
+
+const reviewValidator = () => {
+  return [
+    body('UserId', 'UserId is required and should be a number').isNumeric(),
+    body('Title', 'Title of the review is required').notEmpty(),
+    body('Content', 'Content of the review is required').notEmpty(),
+    body('Rating', 'Rating is required and should be a number between 1 and 5')
+      .isNumeric()
+      .custom((value) => value >= 1 && value <= 5),
+    body('Date', 'Date is required and should be in ISO format').isISO8601(),
+    body('Verified', 'Verified is required and should be a boolean value').isBoolean(),
+    body('GameId', 'GameId is required and should be a valid ObjectId')
+      .notEmpty()
+      .isLength(24),
+  ];
+};
+
+const reviewSearchValidator = () => {
+  return [
+    param('reviewId', 'Please enter a valid review id').notEmpty().isLength(24),
+  ];
+};
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const errorList = [];
+  errors.array().map((error) => {
+    errorList.push({
+      [error.path]: error.msg,
+    });
+  });
+  return res.status(400).json({
+    errors: errorList,
+  });
+};
+
+module.exports = { reviewValidator, reviewSearchValidator, validate };
