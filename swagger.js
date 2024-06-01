f0663e39ff853d215b20cd063c629a15bedc914c
adcostanzi <andres.costanzi@outlook.com>
2024-06-01 14:23:38 -0500

    swagger host updated to render

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description:
       "An API to provide information about games, developers, publishers and reviews in Json objects",
   },
-  host: "localhost:3000",
+  host: "cse341-final-project-team17.onrender.com",
   schemes: ["https", "http"],
 };
 
